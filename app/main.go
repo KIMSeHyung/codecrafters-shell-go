92c8c06bd57a2aa05007dcd7a42ad8b2111fde3a
KIMSeHyung <tpgud33@gmail.com>
2025-03-05 00:34:40 +0900

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -11,11 +11,14 @@ import (
 var _ = fmt.Fprint
 
 func main() {
+	builtinCommands := map[string]bool{"exit": true, "echo": true, "type": true}
+
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
 
 		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-		cmd := strings.Split(command, " ")
+		cmd := strings.Split(strings.TrimSpace(command), " ")
+
 		if err == nil {
 			switch cmd[0] {
 
@@ -23,7 +26,14 @@ func main() {
 				os.Exit(0)
 
 			case "echo":
-				fmt.Printf("%s", strings.Join(cmd[1:], " "))
+				fmt.Printf("%s\n", strings.Join(cmd[1:], " "))
+
+			case "type":
+				if builtinCommands[cmd[1]] {
+					fmt.Printf("%s is a shell builtin\n", cmd[1])
+				} else {
+					fmt.Printf("%s: not found\n", cmd[1])
+				}
 
 			default:
 				fmt.Printf("%s: command not found\n", command[:len(command)-1])
